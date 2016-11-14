5228e440563d79dd4b8b13d4e18db87b03e55e0c
rgulle4 <rgulle4@lsu.edu>
2016-11-13 18:00:31 -0600

    Add a stuff to test1.scm
    ```scheme
      (define (z x) (b+ x y))
      (define y 42)
      (define x 17)
    
      (z y) ; should return 84
    ```
    
    example run
        cat test1.scm | ./Scheme4101-reference.exe
    output is
       > 8
       > > > > 84
       >

--- test1.scm
+++ test1.scm
@@ -1,2 +1,15 @@
 ;; a test scheme file
 (b+ 3 5) ; should return 8
+
+(define (z x) (b+ x y))
+(define y 42)
+(define x 17)
+
+(z y) ; should return 84
+
+;; example run
+;;     cat test1.scm | ./Scheme4101-reference.exe
+;; output is
+;;     > 8
+;;     > > > > 84
+;;     >

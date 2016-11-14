9e173e2463719300e73606226f6ca3df9cb2a724
rgulle4 <rgulle4@lsu.edu>
2016-11-14 07:36:48 -0600

    Quick formatting fix in Define.cs

--- Special/Define.cs
+++ Special/Define.cs
@@ -11,19 +11,17 @@ namespace Tree {
         public override Node eval(Node exp, Environment env) {
             List<Node> argsList = Node.getArgsList(exp.getCdr());
             int numArgs = argsList.Count;
-
+            
             if (numArgs < 2) 
                 return Node.nilNodeWithErrorMsg("Error: invalid expression");
              
-             if (argsList[0].isSymbol() && numArgs == 2) {
-                 Node id = argsList[0];
-                 Node val = argsList[1].eval(env);
-                 env.define(id, val);
-                 StringLit.SHOULD_PRINT_AT_ALL = false;
-                 return new StringLit("");
-             }
-
-
+            if (argsList[0].isSymbol() && numArgs == 2) {
+                Node id = argsList[0];
+                Node val = argsList[1].eval(env);
+                env.define(id, val);
+                StringLit.SHOULD_PRINT_AT_ALL = false;
+                return new StringLit("");
+            }
             return Node.nilNodeWithErrorMsg("Error: invalid expression");
         }
         

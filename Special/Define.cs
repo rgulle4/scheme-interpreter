6eb8233f33393ee038a21d68bd1c48028b209bb3
rgulle4 <rgulle4@lsu.edu>
2016-11-14 08:33:12 -0600

    STOPPING POINT: define function, and apply
    TODO: implement Closure.apply()
    TODO: finish Define.cs for functions; it's close, but not really:
    
    input:
    
        (define (foo) 1337)
    
    output-ours:
    
    `   #{Procedure
            (lambda ()
                . 1337
            )
         }
    
    output-reference:
    
    `   #{Procedure
            (lambda ()
                1337
            )
         }

--- Special/Define.cs
+++ Special/Define.cs
@@ -11,10 +11,11 @@ namespace Tree {
         public override Node eval(Node exp, Environment env) {
             List<Node> argsList = Node.getArgsList(exp.getCdr());
             int numArgs = argsList.Count;
-            
+
             if (numArgs < 2) 
                 return Node.nilNodeWithErrorMsg("Error: invalid expression");
-             
+            
+            // define a variable
             if (argsList[0].isSymbol() && numArgs == 2) {
                 Node id = argsList[0];
                 Node val = argsList[1].eval(env);
@@ -22,7 +23,44 @@ namespace Tree {
                 StringLit.SHOULD_PRINT_AT_ALL = false;
                 return new StringLit("");
             }
+
+            // TODO: finish define a function
+            if (argsList[0].isPair() && numArgs == 2) {
+                Node fnSignature = argsList[0];
+                Node fnBody = argsList[1];
+
+                Node fnId = fnSignature.getCar();
+                Node fnArgs = fnSignature.getCdr();
+                List<Node> fnArgsList = Node.getArgsList(fnArgs); 
+
+                if (!fnId.isSymbol() || fnArgsAreNotValid(fnArgsList))
+                    return Node.nilNodeWithErrorMsg("Error: ill-formed definition");
+
+                Node id = fnId;
+                Node val = (new Cons(
+                    new Ident("lambda"),
+                    new Cons(fnArgs, fnBody)
+                )).eval(env);
+
+                env.define(id, val);                
+                StringLit.SHOULD_PRINT_AT_ALL = false;
+                return new StringLit("");
+            }
+
             return Node.nilNodeWithErrorMsg("Error: invalid expression");
+        }
+
+        private bool fnArgsAreNotValid(List<Node> fnArgsList) {
+            return !fnArgsAreAllValid(fnArgsList);
+        }
+
+        private bool fnArgsAreAllValid(List<Node> fnArgsList) {
+            bool ok = true;
+            foreach (Node arg in fnArgsList) {
+                if (!arg.isSymbol())
+                    return false;
+            }
+            return ok;
         }
         
         public override void print(Node t, int n, bool p) {
--- Special/Lambda.cs
+++ Special/Lambda.cs
@@ -6,10 +6,8 @@ namespace Tree {
     public class Lambda : Special {
         public Lambda() { }
         
-        // TODO: implement eval
         public override Node eval(Node exp, Environment env) {
-            Console.Error.WriteLine("TODO: eval not implemented");
-            return Nil.getInstance();
+            return new Closure(exp, env);
         }
 
         public override void print(Node t, int n, bool p) {
